7b16c34b45ce2bb4dcc6d97004bc37006945e678
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-05-22 20:44:43 -0600

    uncuried NFT

--- lib/src/nft0/models/UncurriedNft.dart
+++ lib/src/nft0/models/UncurriedNft.dart
@@ -1,4 +1,8 @@
 import 'package:chia_utils/chia_crypto_utils.dart';
+import 'package:chia_utils/src/nft0/puzzles/nft_state_layer/nft_state_layer.clvm.hex.dart';
+import 'package:chia_utils/src/nft0/puzzles/singleton_top_layer_v1_1/singleton_top_layer_v1_1.clvm.hex.dart';
+
+import '../../clvm/keywords.dart';
 
 /// A simple solution for uncurry NFT puzzle.
 
@@ -25,7 +29,7 @@ class UncurriedNFT {
   final Program owner_did;
 
   /// Metadata updater puzzle hash
-  final Program metdata_updater_hash;
+  final Program metadataUpdaterHash;
 
   /// Puzzle hash of the transfer program
   final Program transfer_program_hash;
@@ -48,7 +52,7 @@ class UncurriedNFT {
   ///  NFT state layer inner puzzle
   final Program inner_puzzle;
 
-  UncurriedNFT({
+  UncurriedNFT._({
     required this.nft_mod_hash,
     required this.nft_state_layer,
     required this.singleton_struct,
@@ -56,7 +60,7 @@ class UncurriedNFT {
     required this.singleton_launcher_id,
     required this.launcher_puzhash,
     required this.owner_did,
-    required this.metdata_updater_hash,
+    required this.metadataUpdaterHash,
     required this.transfer_program_hash,
     required this.transfer_program_curry_params,
     required this.royalty_address,
@@ -69,6 +73,85 @@ class UncurriedNFT {
     required this.inner_puzzle,
   });
 
+  static UncurriedNFT uncurry(Program puzzle) {
+    late Program singletonStruct;
+    late Program nftStateLayer;
+    late Program sinletonModHash;
+    late Program singletonLauncherId;
+    late Program launcherPuzzhash;
+    late Program dataUris;
+    late Program dataHash;
+
+    final uncurried = puzzle.uncurry();
+    final mod = uncurried.program;
+    final curried_args = uncurried.arguments;
+    if (mod.toSource() != singletonTopLayerProgram.toSource()) {
+      throw ArgumentError(
+          "Cannot uncurry NFT puzzle, failed on singleton top layer: Mod ${mod}");
+    }
+    try {
+      singletonStruct = curried_args[0];
+      nftStateLayer = curried_args[1];
+      sinletonModHash = singletonStruct.first();
+      singletonLauncherId = singletonStruct.rest().first();
+      launcherPuzzhash = singletonStruct.rest().rest();
+    } catch (e) {
+      throw ArgumentError(
+          "Cannot uncurry singleton top layer: Args ${curried_args}");
+    }
+
+    //TODO [curried_args] maybe would be has the method [rest], but not found, is posible the solution if corect
+
+    final uncurriedNft = curried_args[1].first().uncurry();
+    final nftMod = uncurriedNft.program;
+    final nftArgs = uncurriedNft.arguments;
+    if (nftMod.toSource() != nftStateLayerProgram.toSource()) {
+      throw ArgumentError(
+          "Cannot uncurry NFT puzzle, failed on NFT state layer: Mod ${mod}");
+    }
+    try {
+      final nftModHash = nftArgs[0];
+      final metadata = nftArgs[1];
+      final metadataUpdaterHash = nftArgs[2];
+      final innerPuzzleHash = nftArgs[3];
+
+      for (var kvPair in metadata.toList()) {
+        if (bytesEqual(kvPair.first().atom, encodeBigInt(keywords['u']!))) {
+          dataUris = kvPair.rest();
+        }
+        if (bytesEqual(kvPair.first().atom, encodeBigInt(keywords['h']!))) {
+          dataHash = kvPair.rest();
+        }
+      }
+      return UncurriedNFT._(
+        nft_mod_hash: nftModHash,
+        nft_state_layer: nftStateLayer,
+        singleton_struct: singletonStruct,
+        singleton_mod_hash: sinletonModHash,
+        singleton_launcher_id: singletonLauncherId,
+        launcher_puzhash: launcherPuzzhash,
+        metadata: metadata,
+        data_uris: dataUris,
+        data_hash: dataHash,
+        inner_puzzle: innerPuzzleHash,
+        metadataUpdaterHash: metadataUpdaterHash,
+
+        // TODO Set/Remove following fields after NFT1 implemented
+
+        owner_did: Program.fromBytes([]),
+
+        transfer_program_hash: Program.fromBytes([]),
+        transfer_program_curry_params: Program.fromBytes([]),
+        royalty_address: Program.fromBytes([]),
+        trade_price_percentage: Program.fromBytes([]),
+        settlement_mod_hash: Program.fromBytes([]),
+        cat_mod_hash: Program.fromBytes([]),
+      );
+    } catch (e) {
+      throw Exception("Cannot uncurry NFT state layer: Args ${curried_args}");
+    }
+  }
+
   UncurriedNFT copyWith({
     Program? nft_mod_hash,
     Program? nft_state_layer,
@@ -77,7 +160,7 @@ class UncurriedNFT {
     Program? singleton_launcher_id,
     Program? launcher_puzhash,
     Program? owner_did,
-    Program? metdata_updater_hash,
+    Program? metadataUpdaterHash,
     Program? transfer_program_hash,
     Program? transfer_program_curry_params,
     Program? royalty_address,
@@ -89,7 +172,7 @@ class UncurriedNFT {
     Program? data_hash,
     Program? inner_puzzle,
   }) {
-    return UncurriedNFT(
+    return UncurriedNFT._(
       nft_mod_hash: nft_mod_hash ?? this.nft_mod_hash,
       nft_state_layer: nft_state_layer ?? this.nft_state_layer,
       singleton_struct: singleton_struct ?? this.singleton_struct,
@@ -98,7 +181,7 @@ class UncurriedNFT {
           singleton_launcher_id ?? this.singleton_launcher_id,
       launcher_puzhash: launcher_puzhash ?? this.launcher_puzhash,
       owner_did: owner_did ?? this.owner_did,
-      metdata_updater_hash: metdata_updater_hash ?? this.metdata_updater_hash,
+      metadataUpdaterHash: metadataUpdaterHash ?? this.metadataUpdaterHash,
       transfer_program_hash:
           transfer_program_hash ?? this.transfer_program_hash,
       transfer_program_curry_params:
