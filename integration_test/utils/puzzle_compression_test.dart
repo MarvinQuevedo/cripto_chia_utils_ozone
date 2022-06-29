ae8684e96a42d578cf418ccdacfd47b8a89fdb67
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-06-29 10:52:24 -0600

    finished compress puzzle funcions

---
+++ integration_test/utils/puzzle_compression_test.dart
@@ -0,0 +1,76 @@
+import 'package:chia_crypto_utils/chia_crypto_utils.dart';
+import 'package:chia_crypto_utils/src/offert/puzzles/settlement_payments/settlement_payments.clvm.hex.dart';
+import 'package:chia_crypto_utils/src/offert/utils/puzzle_compression.dart';
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  final keychainSecret = KeychainCoreSecret.generate();
+
+  final walletsSetList = <WalletSet>[];
+  for (var i = 0; i < 2; i++) {
+    final set1 = WalletSet.fromPrivateKey(keychainSecret.masterPrivateKey, i);
+    walletsSetList.add(set1);
+  }
+
+  final keychain = WalletKeychain.fromWalletSets(walletsSetList);
+
+  final zDict = zDictForVersion(LATEST_VERSION);
+
+  final ZERO_32 = Bytes(List.generate(32, (_) => 0));
+  final ONE_32 = Bytes(List.generate(32, (_) => 17));
+  final COIN = CoinPrototype(parentCoinInfo: ZERO_32, puzzlehash: Puzzlehash(ZERO_32), amount: 0);
+  final SOLUTION = Program.list([]);
+
+  void testStandardPuzzle() {
+    final coinSpend = CoinSpend(
+        coin: COIN,
+        puzzleReveal: getPuzzleFromPk(keychain.unhardenedMap.values.toList()[0].childPublicKey),
+        solution: SOLUTION);
+    final compressed = compressObjectWithPuzzles(coinSpend.toBytes(), LATEST_VERSION);
+
+    assert(coinSpend.toBytes().length > compressed.length);
+
+    final coinsSpendUncompressed = CoinSpend.fromBytes(decompressObjectWithPuzzles(compressed));
+    assert(coinsSpendUncompressed == coinSpend);
+    final factor = compressed.length / coinSpend.toBytes().length;
+    print("standart factor = ${factor}");
+  }
+
+  void testCatPuzzle() {
+    final puzzle = CatWalletService.makeCatPuzzle(Program.list([]).hash(),
+        getPuzzleFromPk(keychain.unhardenedMap.values.toList()[0].childPublicKey));
+    final coinSpend = CoinSpend(coin: COIN, puzzleReveal: puzzle, solution: SOLUTION);
+    final compressed = compressObjectWithPuzzles(coinSpend.toBytes(), LATEST_VERSION);
+
+    assert(coinSpend.toBytes().length > compressed.length);
+
+    final coinsSpendUncompressed = CoinSpend.fromBytes(decompressObjectWithPuzzles(compressed));
+    assert(coinsSpendUncompressed == coinSpend);
+    final factor = compressed.length / coinSpend.toBytes().length;
+    print("Cat factor = ${factor}");
+  }
+
+  void testOffertPuzzle() {
+    final coinSpend = CoinSpend(coin: COIN, puzzleReveal: offertProgram, solution: SOLUTION);
+    final compressed = compressObjectWithPuzzles(coinSpend.toBytes(), LATEST_VERSION);
+
+    assert(coinSpend.toBytes().length > compressed.length);
+
+    final coinsSpendUncompressed = CoinSpend.fromBytes(decompressObjectWithPuzzles(compressed));
+    assert(coinsSpendUncompressed == coinSpend);
+    final factor = compressed.length / coinSpend.toBytes().length;
+    print("Offert factor = ${factor}");
+  }
+
+  void testLowestBestVersion() {
+    assert(lowestBestVersion([catProgram.toBytes()]) == 1);
+    assert(lowestBestVersion([offertProgram.toBytes()]) == 2);
+  }
+
+  test('Test Puzzle compression', () async {
+    testStandardPuzzle();
+    testCatPuzzle();
+    testOffertPuzzle();
+    testLowestBestVersion();
+  });
+}
--- lib/src/offert/utils/puzzle_compression.dart
+++ lib/src/offert/utils/puzzle_compression.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 import 'package:chia_crypto_utils/src/nft0/puzzles/nft_transfer_program/nft_transfer_program.clvm.hex.dart';
+import 'package:quiver/iterables.dart';
 import '../../nft0/puzzles/nft_metadata_updater/nft_metadata_updater.clvm.hex.dart';
 import '../../nft0/puzzles/nft_state_layer/nft_state_layer.clvm.hex.dart';
 import '../puzzles/settlement_payments/settlement_payments.clvm.hex.dart';
@@ -17,3 +21,74 @@ final ZDICT = [
       nftTransferProgram.toBytes()),
   // more dictionaries go here
 ];
+
+final LATEST_VERSION = ZDICT.length;
+
+class CompressionVersionError {
+  late final String message;
+  CompressionVersionError(int versionNumber) {
+    message =
+        "The data is compressed with version ${versionNumber} and cannot be parsed. Update software and try again.";
+  }
+}
+
+Bytes zDictForVersion(int version) {
+  Bytes summedDict = Bytes.empty;
+  final subList = ZDICT.sublist(0, version - 1);
+  for (var item in subList) {
+    summedDict += item;
+  }
+  return summedDict;
+}
+
+Bytes compressWithZdict(Bytes blob, Bytes zdict) {
+  final codec = ZLibCodec(dictionary: zdict);
+  final compressObj = codec.encode(blob);
+  return Bytes(compressObj);
+}
+
+Bytes decompressWithZdict(Bytes blob, Bytes zdict) {
+  final codec = ZLibCodec(dictionary: zdict);
+  final compressObj = codec.decode(blob);
+  return Bytes(compressObj);
+}
+
+Bytes compressObjectWithPuzzles(Bytes objectBytes, int version) {
+  final versionBlob = intToBytes(version, 2, Endian.big);
+  final zdict = zDictForVersion(version);
+  final compressedObjectBlob = compressWithZdict(objectBytes, zdict);
+  return versionBlob + compressedObjectBlob;
+}
+
+Bytes decompressObjectWithPuzzles(Bytes compressedObjectBlob) {
+  final blobIterator = compressedObjectBlob.iterator;
+  final version = bytesToInt(blobIterator.extractBytesAndAdvance(2), Endian.big);
+  if (version > LATEST_VERSION) {
+    throw CompressionVersionError(version);
+  }
+  final zdict = zDictForVersion(version);
+  final objectBytes = decompressWithZdict(
+      blobIterator.extractBytesAndAdvance(compressedObjectBlob.length - 2), zdict);
+  return objectBytes;
+}
+
+int lowestBestVersion(List<Bytes> puzzleList, {int? maxVersion}) {
+  if (maxVersion == null) {
+    maxVersion = LATEST_VERSION;
+  }
+  int highestVersion = 1;
+  for (var mod in puzzleList) {
+    for (var v = 0; v < maxVersion; v++) {
+      final version = v + 1;
+      final dict = ZDICT[v];
+      if (version > maxVersion) {
+        break;
+      }
+      if (dict.contains(mod)) {
+        highestVersion = max([highestVersion, version])!;
+      }
+    }
+  }
+
+  return highestVersion;
+}
