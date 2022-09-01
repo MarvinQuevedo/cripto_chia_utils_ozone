d3a36563dd4303bb88a7d59abe0a9d3fc2d5d9e1
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-09-01 15:52:39 -0600

    Fix conditions CLVM code

--- lib/src/nft1.0/puzzles/p2_conditions/p2_conditions.dart
+++ lib/src/nft1.0/puzzles/p2_conditions/p2_conditions.dart
@@ -4,7 +4,7 @@ import '../../../clvm.dart';
 import '../../../clvm/program.dart';
 
 final P2_CONDITIONS_MOD = Program.deserializeHex(
-  "ff02ffff01ff04ffff04ff04ffff04ff05ffff04ffff02ff06ffff04ff02ffff04ff0bff80808080ff80808080ff0b80ffff04ffff01ff32ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff06ffff04ff02ffff04ff09ff80808080ffff02ff06ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080",
+  "ff04ffff0101ff0280",
 );
 
 Program puzzleForConditions(Program conditions) {
--- lib/src/nft1.0/service/index.dart
+++ lib/src/nft1.0/service/index.dart
@@ -3,3 +3,4 @@ export 'metadata_outer_puzzle.dart';
 export 'ownership_outer_puzzle.dart';
 export 'singleton_outer_puzzle.dart';
 export 'transfer_program_puzzle.dart';
+export 'nft_puzzle.dart';
