8ba6bbd7e065005299ddb689488a8875ec4f04c9
Marvin Quevedo <marvinquevedo2017@outlook.es>
2022-09-05 15:25:47 -0600

    Parse from bytes from blockchain

--- lib/src/core/models/coin.dart
+++ lib/src/core/models/coin.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'dart:typed_data';
+
 import 'package:chia_crypto_utils/chia_crypto_utils.dart';
 import 'package:chia_crypto_utils/src/utils/serialization.dart';
 import 'package:meta/meta.dart';
@@ -26,6 +28,25 @@ class Coin extends CoinPrototype with ToBytesMixin {
           amount: amount,
           parentCoinInfo: parentCoinInfo,
         );
+
+  Coin copyWith({
+    int? confirmedBlockIndex,
+    int? spentBlockIndex,
+    bool? coinbase,
+    int? timestamp,
+    Bytes? parentCoinInfo,
+    Puzzlehash? puzzlehash,
+    int? amount,
+  }) {
+    return Coin(
+        confirmedBlockIndex: confirmedBlockIndex ?? this.confirmedBlockIndex,
+        spentBlockIndex: spentBlockIndex ?? this.spentBlockIndex,
+        coinbase: coinbase ?? this.coinbase,
+        timestamp: timestamp ?? this.timestamp,
+        amount: amount ?? this.amount,
+        parentCoinInfo: parentCoinInfo ?? this.parentCoinInfo,
+        puzzlehash: puzzlehash ?? this.puzzlehash);
+  }
 
   factory Coin.fromChiaCoinRecordJson(Map<String, dynamic> json) {
     final coinPrototype = CoinPrototype.fromJson(json['coin'] as Map<String, dynamic>);
@@ -76,6 +97,26 @@ class Coin extends CoinPrototype with ToBytesMixin {
       spentBlockIndex: spentBlockIndex,
       coinbase: coinbase,
       timestamp: timestamp,
+      parentCoinInfo: coinPrototype.parentCoinInfo,
+      puzzlehash: coinPrototype.puzzlehash,
+      amount: coinPrototype.amount,
+    );
+  }
+
+  factory Coin.fromBlokchainBytes(Bytes bytes) {
+    final bytesIter = bytes.iterator;
+    final coinPrototype = CoinPrototype.fromStream(bytesIter);
+
+    final confirmedBlockIndex = bytesToInt(bytesIter.extractBytesAndAdvance(4), Endian.big);
+    final spentBlockIndex = bytesToInt(bytesIter.extractBytesAndAdvance(4), Endian.big);
+    final coinbase = bytesToInt(bytesIter.extractBytesAndAdvance(1), Endian.big) == 1;
+    final timestamp = bytesToBigInt(bytesIter.extractBytesAndAdvance(8), Endian.big);
+
+    return Coin(
+      confirmedBlockIndex: confirmedBlockIndex,
+      spentBlockIndex: spentBlockIndex,
+      coinbase: coinbase,
+      timestamp: timestamp.toInt(),
       parentCoinInfo: coinPrototype.parentCoinInfo,
       puzzlehash: coinPrototype.puzzlehash,
       amount: coinPrototype.amount,
