92e271c1c072afe3003e081ee528b7d3595337bd
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-09-23 19:22:39 -0600

    add body to response

--- lib/src/api/full_node/models/responses/chia_base_response.dart
+++ lib/src/api/full_node/models/responses/chia_base_response.dart
@@ -4,16 +4,19 @@ import 'package:meta/meta.dart';
 class ChiaBaseResponse {
   final String? error;
   final bool success;
+  final Map<String, dynamic>? body;
 
   const ChiaBaseResponse({
     required this.error,
     required this.success,
+    this.body,
   });
 
   factory ChiaBaseResponse.fromJson(Map<String, dynamic> json) {
     return ChiaBaseResponse(
       error: json['error'] as String?,
       success: json['success'] as bool? ?? false,
+      body: json['body'] as Map<String, dynamic>?,
     );
   }
   Map<String, dynamic> toJson() => <String, dynamic>{
